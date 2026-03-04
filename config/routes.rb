3245a3e505c71c4e49bdcc5a82b4217c1c5608b7
manu raffard <manu_raffard@hotmail.com>
2026-03-04 10:36:16 +0100

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   resources :interviews, only: [:index, :show] do
-  resources :chats, only: [:create]
-end
-
-resources :chats, only: [:show] do
-  resources :messages, only: [:create]
-end
+    resources :chats, only: [:create]
+  end
 end
